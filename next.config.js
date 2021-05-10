9ef13cc720a363e6bfef9ada04c80098b07f72a2
Kavin valli <kavinvalli@gmail.com>
2021-05-10 10:48:02 +0530

    Add Deployment to gh-pages pipeline

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
+
 module.exports = {
   future: {
     webpack5: true,
   },
+  assetPrefix: ghPages ? "/new-delhi-space-society.github.io/" : "",
 };
