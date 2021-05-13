3f0d5bb6c29335b55786d75bfc0838963f62afab
Kavin valli <kavinvalli@gmail.com>
2021-05-13 23:46:10 +0530

    Remove data fetching with api(not supported with next export). Use data directly

--- pages/members.tsx
+++ pages/members.tsx
@@ -1,11 +1,19 @@
-import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { server } from "../config";
+// import { GetStaticProps, InferGetStaticPropsType } from "next";
+// import { server } from "../config";
+import { members } from "../data/members";
 import { MembersData } from "./api/members";
 import MembersContent from "../components/members/MembersContent";
 
-export default function Members({
-  members,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+// export default function Members({
+//   members,
+// }: InferGetStaticPropsType<typeof getStaticProps>) {
+//   return (
+//     <main style={{ minHeight: "calc(100vh - 150px)" }}>
+//       <MembersContent members={members} />
+//     </main>
+//   );
+// }
+export default function Members() {
   return (
     <main style={{ minHeight: "calc(100vh - 150px)" }}>
       <MembersContent members={members} />
@@ -17,22 +25,22 @@ interface iProps {
   members?: MembersData;
 }
 
-export const getStaticProps: GetStaticProps<iProps> = async () => {
-  console.log("RUNNING");
-  console.log(server);
-  const res = await fetch(`${server}/api/members`);
-  if (!res.ok) {
-    return {
-      props: {
-        members: [],
-      },
-    };
-  }
-  const members: MembersData = await res.json();
+// export const getStaticProps: GetStaticProps<iProps> = async () => {
+//   console.log("RUNNING");
+//   console.log(server);
+//   const res = await fetch(`${server}/api/members`);
+//   if (!res.ok) {
+//     return {
+//       props: {
+//         members: [],
+//       },
+//     };
+//   }
+//   const members: MembersData = await res.json();
 
-  return {
-    props: {
-      members,
-    },
-  };
-};
+//   return {
+//     props: {
+//       members,
+//     },
+//   };
+// };
