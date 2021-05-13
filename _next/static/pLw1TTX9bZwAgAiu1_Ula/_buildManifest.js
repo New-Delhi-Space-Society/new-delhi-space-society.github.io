0f29d968ce4080fe9ca27fd5127ee25a52ad64b1
kavin25 <41034356+kavin25@users.noreply.github.com>
2021-05-13 07:27:07 +0000

    Deploying to gh-pages from  @ 91119f82d57552bac9f24edc98c9b1bcebc455eb 🚀

--- _next/static/pLw1TTX9bZwAgAiu1_Ula/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e280c484753ee0d49b2f.js"],"/_error":["static/chunks/pages/_error-4b6072ad18b09704d2fb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
