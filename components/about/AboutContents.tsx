c7ff72717f827ac92074ba0c2eb066ce616586f5
Kavin valli <kavinvalli@gmail.com>
2021-06-14 22:54:14 +0530

    feat(content): Change home and about content

--- components/about/AboutContents.tsx
+++ components/about/AboutContents.tsx
@@ -16,25 +16,40 @@ const HistoryContainer = styled.div`
   }
 `;
 
+const PrincipalsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  @media ${device.lg} {
+    display: block;
+    text-align: left;
+  }
+`;
+
 export default function AboutContents() {
   return (
     <AppContainer>
       <SubHeadingContents title="History">
         <HistoryContainer>
           <div className="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non
-            ultrices et elit lectus. At facilisi et pulvinar est, duis nunc
-            dictum. Orci, elit nunc, vel, pellentesque turpis risus in dolor.
-            Volutpat turpis suspendisse accumsan velit. Senectus vel feugiat at
-            vitae commodo eu scelerisque enim laoreet. Amet mi urna fermentum
-            ullamcorper sollicitudin.
+            The New Delhi Space Society is a non-profit organization and
+            functions as a chapter of the National Space Society. It was founded
+            in 2018, seeing the level of enthusiasm in New Delhi students about
+            space exploration and a lack of resources for this enthusiasm. We
+            aim to make Space an accessible resource for everyone.
             <br />
-            <br />A fringilla nulla libero viverra. Ullamcorper aliquam varius
-            magna posuere. Vel blandit sed purus proin eros, netus morbi euismod
-            et. Nisi eu sed fames hac tortor. Libero placerat a pellentesque
-            nunc arcu, suspendisse nibh nisl. Mattis aliquet nunc vel sed
-            sollicitudin gravida sem a. Dolor risus faucibus varius risus
-            sollicitudin id senectus. Diam integer id enim cursus cum iaculis.
+            <br />
+            We intend to do so by utilising our outreach programs and by
+            inculcating awareness about various space-related mysteries and
+            information. This shall pique the curiosity of many-a-students and
+            make the vastness of space more valuable in everyone’s eyes. We
+            share the same mission as the NSS, that is, “to promote social,
+            economic, technological, and political change in order to expand
+            civilization beyond Earth, to settle space and to use the resulting
+            resources to build a hopeful and prosperous future for humanity.”
           </div>
           <img
             style={{ maxWidth: "100%", height: "auto" }}
@@ -43,22 +58,29 @@ export default function AboutContents() {
           />
         </HistoryContainer>
       </SubHeadingContents>
-      <SubHeadingContents title="What do we do?">
-        <div className="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non ultrices
-          et elit lectus. At facilisi et pulvinar est, duis nunc dictum. Orci,
-          elit nunc, vel, pellentesque turpis risus in dolor. Volutpat turpis
-          suspendisse accumsan velit. Senectus vel feugiat at vitae commodo eu
-          scelerisque enim laoreet. Amet mi urna fermentum ullamcorper
-          sollicitudin.
-          <br />
-          <br />A fringilla nulla libero viverra. Ullamcorper aliquam varius
-          magna posuere. Vel blandit sed purus proin eros, netus morbi euismod
-          et. Nisi eu sed fames hac tortor. Libero placerat a pellentesque nunc
-          arcu, suspendisse nibh nisl. Mattis aliquet nunc vel sed sollicitudin
-          gravida sem a. Dolor risus faucibus varius risus sollicitudin id
-          senectus. Diam integer id enim cursus cum iaculis.
-        </div>
+      <SubHeadingContents title="What principals do we follow?">
+        <PrincipalsContainer>
+          <img
+            style={{ maxWidth: "100%", height: "auto" }}
+            src={require("../../public/milky-way.jpeg")}
+            alt="Vector Graphic"
+          />
+          <div className="content">
+            Our motive is to spread awareness about the possibilities of space
+            science and aerospace engineering. We believe that together, we can
+            bring about a difference in society as well as the future of our
+            species through space science. We also strive to act as a bridge
+            between the scientific breakthroughs that take place in the field of
+            aerospace and space science, as well as research about scientific
+            mysteries ourselves.
+            <br />
+            <br />
+            We believe in the Latin phrase “Ad Astra”, as it is our hope that
+            one day humans can successfully work their way through space,
+            navigating and inhabiting stars as well as accomplishing
+            breakthroughs that were once merely dreams.
+          </div>
+        </PrincipalsContainer>
       </SubHeadingContents>
     </AppContainer>
   );
--- components/index/Contents.tsx
+++ components/index/Contents.tsx
@@ -5,13 +5,18 @@ export default function Contents() {
   return (
     <AppContainer>
       <SubHeadingContents title="Introduction">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ultricies
-        purus ullamcorper amet habitant et mauris facilisi. Habitant egestas
-        amet malesuada iaculis duis eleifend maecenas. Et, in arcu semper
-        pulvinar urna cursus sapien. Nullam facilisi venenatis blandit accumsan
-        consectetur. Lectus blandit tortor molestie tortor. Et ultrices
-        fringilla aliquam eget iaculis suscipit. Ut ipsum interdum leo turpis
-        imperdiet fames feugiat eu. Adipiscing libero dapibus.
+        With a goal of making advancement in space science and aerospace
+        engineering, we are a group of space enthusiasts who formed the New
+        Delhi Space Society, a chapter of{" "}
+        <a href="http://space.nss.org" target="_blank">
+          National Space Society
+        </a>
+        . We conduct research and explore the nuances in space, but we are not
+        limited to that, we also do outreach programs, hold and take part in
+        competitions and various other activities to reach our goal and spread
+        awareness about space. We are a growing group of aerospace aficionados
+        and are actively looking for your support! Learn more in the ‘About Us’
+        section and volunteer your efforts!
       </SubHeadingContents>
       <SubHeadingContents title="SpaceTech">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ultricies
