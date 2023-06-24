fb21834aa4ac8acfe3b3dfe56ad96ba8ce7f1800
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-06-25 03:25:22 +0530

    spacetech

--- pages/spacetech.tsx
+++ pages/spacetech.tsx
@@ -1,9 +1,11 @@
-import spacetechContents from "../components/spacetech/spacetechContents";
-
-export default function spacetech() {
-  return (
-    <main style={{ minHeight: "calc(100vh - 150px)" }}>
-      <spacetechContents />
-    </main>
-  );
-}
+function Spacetech() {
+    return (
+      <main style={{ minHeight: "calc(100vh - 150px)" }}>
+        <h1>Lorem ipsum</h1>
+        <p>Spacetech?</p>
+      </main>
+    );
+  }
+  
+  export default Spacetech;
+  
