7de4811b099e3bf98fa2b1ac7eb7012e67d14537
Nisarga Adhikary <git@nisarga.me>
2023-07-11 00:23:09 +0530

    Update SubHeadingContents.tsx

--- components/SubHeadingContents.tsx
+++ components/SubHeadingContents.tsx
@@ -1,4 +1,4 @@
-import { KH2, KBody } from "./Typography";
+import { KH2, KBody, lightTheme } from "./Typography";
 
 interface SubHeadingContentsProps {
   title: string;
@@ -7,7 +7,9 @@ interface SubHeadingContentsProps {
 const SubHeadingContents: React.FC<SubHeadingContentsProps> = ({ title, children }) => {
   return (
     <div className="content">
-      <KH2 style={{ margin: "32px 0 8px 0" }}>{title}</KH2>
+      <KH2 style={{ margin: "32px 0 8px 0", color: lightTheme.colors.secondaryMain }}>
+        {title}
+      </KH2>
       <KBody>{children}</KBody>
     </div>
   );
