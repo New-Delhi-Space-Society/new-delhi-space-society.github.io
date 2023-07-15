760ec87884044ed2588bf1ad4e2e1d358bfd2d0e
Nisarga Adhikary <hello@nisarga.me>
2023-07-15 12:58:15 +0530

    Update MemberCard.tsx

--- components/members/MemberCard.tsx
+++ components/members/MemberCard.tsx
@@ -9,9 +9,10 @@ interface iProps {
 
 const MemberCardContainer = styled.div`
   text-align: center;
-  box-shadow: 0px 8px 24px 16px rgba(112, 144, 176, 0.10);
+  box-shadow: 0px 8px 24px 16px rgba(112, 144, 176, 0.2);
   border-radius: 44px;
   width: 300px;
+  position: relative;
   height: 200px;
   padding: 95px;
   margin-bottom: 30px;
@@ -23,20 +24,6 @@ const MemberCardContainer = styled.div`
 
   @media ${device.lg} {
     margin-bottom: 0;
-  }
-
-  .MemberImage {
-    width: 200px;
-    height: 200px;
-    border-radius: 0;
-    position: relative;
-    transform: translate(-50%, -50%);
-  }
-
-  .MemberImage img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
   }
 `;
 
