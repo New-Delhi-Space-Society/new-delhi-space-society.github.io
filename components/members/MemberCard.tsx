f66e163c5335826ea8cd0fb7d8bf0487e0f0275a
Nisarga Adhikary <git@nisarga.me>
2023-07-15 10:32:23 +0530

    Update MemberCard.tsx

--- components/members/MemberCard.tsx
+++ components/members/MemberCard.tsx
@@ -10,16 +10,20 @@ interface iProps {
 const MemberCardContainer = styled.div`
   text-align: center;
   box-shadow: 0px 8px 24px 16px rgba(112, 144, 176, 0.1);
-  border-radius: 0;
+  border-radius: 24px;
   width: 232.064px;
   height: 334.262px;
   flex-shrink: 0;
-  background-color: #FFF;
 `;
 
 const MemberImage = styled.img`
-  width: 232.064px;
+  width: 166px;
   height: 166px;
+  border-radius: 50%;
+  position: absolute;
+  left: 50%;
+  top: 0;
+  transform: translate(-50%, -50%);
 `;
 
 const MemberCard: React.FunctionComponent<iProps> = ({ member }) => {
