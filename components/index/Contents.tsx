4f6b7ea5be6cd76c2e394f56fa8ed548e240a9b3
Nisarga Adhikary <hello@nisarga.me>
2023-07-15 10:47:50 +0530

    Update Contents.tsx

--- components/index/Contents.tsx
+++ components/index/Contents.tsx
@@ -27,11 +27,6 @@ export default function Contents() {
       <SubHeadingContents title="Newspaper Features">
         <KBody>New Delhi Space Society has been featured in newspapers like Hindustan Times.</KBody>
       </SubHeadingContents>
-      <SubHeadingContents title="SpaceTech">
-        <KBody>
-          Progress in technology means pushing the boundaries of what is possible, not only on Earth but also in the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions that threaten their survival, computer systems and technology also evolve and become more sophisticated when subjected to extraordinary circumstances. And what is a more strenuous and demanding environment than space? With this in mind, Exun Clan collaborates with the New Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
-        </KBody>
-      </SubHeadingContents>
     </AppContainer>
   );
 }
