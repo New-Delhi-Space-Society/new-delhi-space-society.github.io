9f001267df3e0656f3e302ad1cdf03813dec5de4
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-07-28 17:43:30 +0530

    placeholder

--- components/spacetech/SpacetechContents.tsx
+++ components/spacetech/SpacetechContents.tsx
@@ -43,49 +43,30 @@ const LogoImage = styled.img`
 export default function SpacetechContents() {
   return (
     <AppContainer>
-      <SubHeadingContents title="History">
+      <SubHeadingContents title="What is Spacetech?">
         <HistoryContainer>
           <div className="content" style={{ marginRight: "4px" }}>
-            The New Delhi Space Society is a non-profit organization and
-            functions as a chapter of the National Space Society. It was founded
-            in 2018, seeing the level of enthusiasm in New Delhi students about
-            space exploration and a lack of resources for this enthusiasm. We
-            aim to make Space an accessible resource for everyone.
+          Progress in technology means pushing the boundaries of what is possible, not only on earth, but also the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions which threaten their survival, computer systems and technology too evolve and become more sophisticated if subjected to extraordinary circumstances. And what is a more strenuous, more demanding environment than space? With this in mind, Exun Clan collaborates with the New Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
+          
             <br />
             <br />
-            We intend to do so by utilising our outreach programs and by
-            inculcating awareness about various space-related mysteries and
-            information. This shall pique the curiosity of many-a-students and
-            make the vastness of space more valuable in everyone’s eyes. We
-            share the same mission as the NSS, that is, “to promote social,
-            economic, technological, and political change in order to expand
-            civilization beyond Earth, to settle space and to use the resulting
-            resources to build a hopeful and prosperous future for humanity.”
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae sodales neque, in vehicula erat. Integer eleifend ut felis sed dapibus. Vestibulum non leo libero. Curabitur sed leo eleifend leo ullamcorper volutpat quis at nisl. Sed quis felis fringilla, malesuada massa eget, viverra sem. Etiam lacinia aliquet lorem eget condimentum. Sed in neque sed urna condimentum blandit. Vestibulum vitae finibus lacus. Sed eleifend diam turpis, non consectetur arcu suscipit sed. In dictum massa at magna vehicula, ac varius velit tempor.
           </div>
-          <LogoImage src={"logo-1.png"} alt="Vector Graphic" />
+          <LogoImage src={"https://d23qowwaqkh3fj.cloudfront.net/wp-content/uploads/2022/05/placeholder.png"} alt="Vector Graphic" />
         </HistoryContainer>
       </SubHeadingContents>
       <SubHeadingContents title="What principals do we follow?">
         <PrincipalsContainer>
-          <img
-            style={{ maxWidth: "100%", height: "auto" }}
-            src={"milky-way.jpeg"}
-            alt="Vector Graphic"
-          />
+    
           <div className="content">
-            Our motive is to spread awareness about the possibilities of space
-            science and aerospace engineering. We believe that together, we can
-            bring about a difference in society as well as the future of our
-            species through space science. We also strive to act as a bridge
-            between the scientific breakthroughs that take place in the field of
-            aerospace and space science, as well as research about scientific
-            mysteries ourselves.
+          Aenean sollicitudin pharetra dapibus. Aliquam erat volutpat. Praesent non enim augue. Cras porttitor aliquet nulla, nec mollis diam tempus ac. Mauris non pellentesque nibh, et mattis odio. Donec sit amet dui condimentum, sollicitudin quam sed, dignissim nisl. Cras est risus, varius id mauris quis, elementum varius metus. Maecenas sit amet justo ut tortor tristique hendrerit a id urna. Praesent consectetur, elit vitae lacinia condimentum, dui lectus semper turpis, ut luctus augue purus non lorem. Nam et nulla eu dolor lobortis hendrerit ac vel massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean turpis magna, blandit ut ex sit amet, vestibulum tempus orci.
+
+
             <br />
             <br />
-            We believe in the Latin phrase “Ad Astra”, as it is our hope that
-            one day humans can successfully work their way through space,
-            navigating and inhabiting stars as well as accomplishing
-            breakthroughs that were once merely dreams.
+            Aenean sollicitudin pharetra dapibus. Aliquam erat volutpat. Praesent non enim augue. Cras porttitor aliquet nulla, nec mollis diam tempus ac. Mauris non pellentesque nibh, et mattis odio. Donec sit amet dui condimentum, sollicitudin quam sed, dignissim nisl. Cras est risus, varius id mauris quis, elementum varius metus. Maecenas sit amet justo ut tortor tristique hendrerit a id urna. Praesent consectetur, elit vitae lacinia condimentum, dui lectus semper turpis, ut luctus augue purus non lorem. Nam et nulla eu dolor lobortis hendrerit ac vel massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean turpis magna, blandit ut ex sit amet, vestibulum tempus orci.
+
+
           </div>
         </PrincipalsContainer>
       </SubHeadingContents>
