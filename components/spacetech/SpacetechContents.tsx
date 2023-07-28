ba726502e74fa5a3a63d5980aa965f61f680050b
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-07-28 17:34:16 +0530

    spacetech page?

---
+++ components/spacetech/SpacetechContents.tsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import AppContainer from "../AppContainer";
+import SubHeadingContents from "../SubHeadingContents";
+import { device } from "../../ThemeConfig";
+
+const HistoryContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column-reverse;
+  align-items: center;
+  text-align: center;
+
+  @media ${device.lg} {
+    flex-direction: row;
+    text-align: left;
+  }
+`;
+
+const PrincipalsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  @media ${device.lg} {
+    display: block;
+    text-align: left;
+  }
+`;
+
+const LogoImage = styled.img`
+  max-width: 100%;
+  height: auto;
+  margin-left: 0px;
+
+  @media ${device.lg} {
+    max-width: 40%;
+    margin-left: 10px;
+  }
+`;
+
+export default function SpacetechContents() {
+  return (
+    <AppContainer>
+      <SubHeadingContents title="History">
+        <HistoryContainer>
+          <div className="content" style={{ marginRight: "4px" }}>
+            The New Delhi Space Society is a non-profit organization and
+            functions as a chapter of the National Space Society. It was founded
+            in 2018, seeing the level of enthusiasm in New Delhi students about
+            space exploration and a lack of resources for this enthusiasm. We
+            aim to make Space an accessible resource for everyone.
+            <br />
+            <br />
+            We intend to do so by utilising our outreach programs and by
+            inculcating awareness about various space-related mysteries and
+            information. This shall pique the curiosity of many-a-students and
+            make the vastness of space more valuable in everyone’s eyes. We
+            share the same mission as the NSS, that is, “to promote social,
+            economic, technological, and political change in order to expand
+            civilization beyond Earth, to settle space and to use the resulting
+            resources to build a hopeful and prosperous future for humanity.”
+          </div>
+          <LogoImage src={"logo-1.png"} alt="Vector Graphic" />
+        </HistoryContainer>
+      </SubHeadingContents>
+      <SubHeadingContents titsle="What principals do we follow?">
+        <PrincipalsContainer>
+          <img
+            style={{ maxWidth: "100%", height: "auto" }}
+            src={"milky-way.jpeg"}
+            alt="Vector Graphic"
+          />
+          <div className="content">
+            Our motive is to spread awareness about the possibilities of space
+            science and aerospace engineering. We believe that together, we can
+            bring about a difference in society as well as the future of our
+            species through space science. We also strive to act as a bridge
+            between the scientific breakthroughs that take place in the field of
+            aerospace and space science, as well as research about scientific
+            mysteries ourselves.
+            <br />
+            <br />
+            We believe in the Latin phrase “Ad Astra”, as it is our hope that
+            one day humans can successfully work their way through space,
+            navigating and inhabiting stars as well as accomplishing
+            breakthroughs that were once merely dreams.
+          </div>
+        </PrincipalsContainer>
+      </SubHeadingContents>
+    </AppContainer>
+  );
+}
--- pages/spacetech.tsx
+++ pages/spacetech.tsx
@@ -1,49 +1,12 @@
-import styled from "styled-components";
+import SpacetechContents from "../components/spacetech/SpacetechContents";
+import AboutContents from "../components/spacetech/SpacetechContents";
 
-const SpacetechPage = styled.main`
-  minHeight: calc(100vh - 150px);
-  text-align: center;
-
-  h1 {
-    font-size: 2rem;
-    margin-bottom: 0.5rem;
-  }
-
-  h3 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-  }
-
-  p {
-    font-size: 1rem;
-    line-height: 1;
-    margin-bottom: 0rem;
-  }
-`;
-
-function Spacetech() {
+export default function About() {
   return (
-    <SpacetechPage>
-      <h1>SpaceTech</h1>
-      <h4>SpaceTech is an event for space enthusiasts.</h4>
-      <p>
-        Progress in technology means pushing the boundaries of what is possible,
-        not only on earth, but also the final frontier — space. Just as
-        biological systems undergo evolution when subjected to strenuous
-        conditions which threaten their survival, computer systems and
-        technology too evolve and become more sophisticated if subjected to
-        extraordinary circumstances. And what is a more strenuous, more
-        demanding environment than space?
-      </p>
-      <p>
-        With this in mind, Exun Clan collaborates with the New Delhi Chapter
-        of the prestigious National Space Society (USA) to give students the
-        opportunity to explore the realms of space and its concurrence with
-        technology. The event typically has multiple challenging rounds,
-        building up to a final presentation round — with a twist!
-      </p>
-    </SpacetechPage>
+    <main style={{ minHeight: "calc(100vh - 150px)" }}>
+      <SpacetechContents />
+    </main>
   );
 }
 
-export default Spacetech;
+
