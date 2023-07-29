3871926befd40d08668d7abe6f2dcbde6f7c7af5
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-07-30 02:35:18 +0530

    Update SpacetechContents.tsx

--- components/spacetech/SpacetechContents.tsx
+++ components/spacetech/SpacetechContents.tsx
@@ -38,9 +38,7 @@ export default function SpacetechContents() {
             Progress in technology means pushing the boundaries of what is possible, not only on earth, but also the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions which threaten their survival, computer systems and technology too evolve and become more sophisticated if subjected to extraordinary circumstances. And what is a more strenuous, more demanding environment than space? With this in mind, Exun Clan collaborates with the New Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
 
             </p>
-            <p>
-              Fusce eu nisi nec nisl feugiat faucibus eget sit amet sapien. Vestibulum ullamcorper sem non neque consequat bibendum. Nulla bibendum, ex vel pharetra sagittis, neque odio varius tellus, vel accumsan nulla mauris et purus. Cras tincidunt felis nec mauris hendrerit, eu venenatis mi faucibus.
-            </p>
+            
           </div>
           <Slideshow /> 
         </HistoryContainer>
