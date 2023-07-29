fd63307d6d36c30a3aca732b08a1b39f80a8558d
Nisarga Adhikary <git@nisarga.me>
2023-07-29 17:35:20 +0530

    Update SpacehackContents.tsx

--- components/spacehack/SpacehackContents.tsx
+++ components/spacehack/SpacehackContents.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import AppContainer from "../AppContainer";
 import SubHeadingContents from "../SubHeadingContents";
 import { device } from "../../ThemeConfig";
-import Slideshow from "./Slideshow"; // Import the Slideshow component
+import Slideshow from "../Slideshow"; // Import the Slideshow component
 
 const HistoryContainer = styled.div`
   display: flex;
