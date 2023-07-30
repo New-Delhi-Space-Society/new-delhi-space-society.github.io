285e47f4c39815f98f7bd0d25e605b5c2a3108cb
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-07-30 20:28:57 +0530

    Update slideshow.tsx

--- components/slideshow.tsx
+++ components/slideshow.tsx
@@ -18,34 +18,10 @@ const Slideshow = () => {
     return () => clearInterval(interval);
   }, []);
 
-  const goToPrevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      (prevIndex - 1 + imageUrls.length) % imageUrls.length
-    );
-  };
-
-  const goToNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
-  };
-
-  const buttonStyles = {
-    padding: "12px 24px",
-    fontSize: "18px",
-    borderRadius: "5px",
-    backgroundColor: "#007bff",
-    color: "#ffffff",
-    border: "none",
-    cursor: "pointer",
-    outline: "none",
-    boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.2)",
-    transition: "background-color 0.2s ease",
-  };
-
   return (
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         maxWidth: "1200px",
@@ -59,17 +35,6 @@ const Slideshow = () => {
         alt={`Slideshow ${currentImageIndex}`}
         style={{ width: "100%", height: "100%", objectFit: "cover" }}
       />
-      <div style={{ marginTop: "10px" }}>
-        <button onClick={goToPrevImage} style={buttonStyles}>
-          Previous
-        </button>
-        <button
-          onClick={goToNextImage}
-          style={{ ...buttonStyles, marginLeft: "10px" }}
-        >
-          Next
-        </button>
-      </div>
     </div>
   );
 };
