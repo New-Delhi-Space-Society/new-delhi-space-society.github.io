b2656738360ef0fd559847a9166fae3035b37a09
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-08-14 18:29:16 +0530

    Update Contents.tsx

--- components/index/Contents.tsx
+++ components/index/Contents.tsx
@@ -7,11 +7,11 @@ import { KBody } from "../Typography";
 export default function Contents() {
   const introductionContent = (
     <>
-      With the aim of advancing space science and aerospace engineering, we are a passionate group of space enthusiasts who founded the New Delhi Space Society, a distinguished chapter of the{" "}   
+      With the aim of advancing space science and engineering and taking humanity to the stars, we are a passionate group of space enthusiasts at the New Delhi Space Society, a distinguished award-winning chapter of the{" "}
       <a href={nssUrl} target="_blank" rel="noopener noreferrer">
          National Space Society
       </a>
-      . Our mission is to conduct cutting-edge research and explore the wonders of space. However, our pursuits go beyond research; we actively engage in outreach programs, participate in competitions, and host various activities to fulfill our objectives and promote space awareness. As our group of aerospace aficionados continues to grow, we warmly welcome your support! Discover more about us in the About Us section and join us in our journey of exploration and discovery!
+      . Our mission is to conduct outreach programs through a variety, participate in space events at a global stage and lend our voices to the conversation, and host various activities to fulfill our objectives and promote space awareness. As our organization continues to grow, we warmly welcome your support! Discover more about us in the About Us section and join us in our journey of exploration and discovery, we hope to see you with us among the stars 🚀.
     </>
   );
 
