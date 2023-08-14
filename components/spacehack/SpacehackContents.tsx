a84a81ed866bdf2f7d82eb05b68ebc3a1b275c4a
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-08-15 05:21:38 +0530

    alrighr <3

--- components/spacehack/SpacehackContents.tsx
+++ components/spacehack/SpacehackContents.tsx
@@ -47,27 +47,16 @@ export default function SpacehackContents() {
         <HistoryContainer>
           <div className="content" style={{ marginRight: "4px" }}>
           Progress in technology means pushing the boundaries of what is possible, not only on earth, but also the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions which threaten their survival, computer systems and technology too evolve and become more sophisticated if subjected to extraordinary circumstances. And what is a more strenuous, more demanding environment than space? With this in mind, Exun Clan collaborates with the New Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
+
+          SpaceHack@ESYA 2023 is a fast-paced aerospace-focused startup pitch competition, that puts you into the suits of the heads of startups, competing to develop your product, bring it to market, and grow the company as if the sky is not the limit.
+
+Will your crew reach the stars, burn up in the atmosphere, and go out with a boom as all your skills are tested, including innovation, design, marketing, and pitching? Sign up at <a href="https://unstop.com/p/spacehack-esya23-indraprastha-institute-of-information-technology-iiit-delhi-728723"> Unstop </a>, don your suits, check all safety systems, and prepare for lift-off on the 25th of August 2023 at IIIT Delhi!
           
             <br />
             <br />
           </div>
           <LogoImage src={"https://d23qowwaqkh3fj.cloudfront.net/wp-content/uploads/2022/05/placeholder.png"} alt="Vector Graphic" />
         </HistoryContainer>
-      </SubHeadingContents>
-      <SubHeadingContents title="What principles do we follow?">
-        <PrincipalsContainer>
-    
-          <div className="content">
-          Aenean sollicitudin pharetra dapibus. Aliquam erat volutpat. Praesent non enim augue. Cras porttitor aliquet nulla, nec mollis diam tempus ac. Mauris non pellentesque nibh, et mattis odio. Donec sit amet dui condimentum, sollicitudin quam sed, dignissim nisl. Cras est risus, varius id mauris quis, elementum varius metus. Maecenas sit amet justo ut tortor tristique hendrerit a id urna. Praesent consectetur, elit vitae lacinia condimentum, dui lectus semper turpis, ut luctus augue purus non lorem. Nam et nulla eu dolor lobortis hendrerit ac vel massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean turpis magna, blandit ut ex sit amet, vestibulum tempus orci.
-
-
-            <br />
-            <br />
-            Aenean sollicitudin pharetra dapibus. Aliquam erat volutpat. Praesent non enim augue. Cras porttitor aliquet nulla, nec mollis diam tempus ac. Mauris non pellentesque nibh, et mattis odio. Donec sit amet dui condimentum, sollicitudin quam sed, dignissim nisl. Cras est risus, varius id mauris quis, elementum varius metus. Maecenas sit amet justo ut tortor tristique hendrerit a id urna. Praesent consectetur, elit vitae lacinia condimentum, dui lectus semper turpis, ut luctus augue purus non lorem. Nam et nulla eu dolor lobortis hendrerit ac vel massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean turpis magna, blandit ut ex sit amet, vestibulum tempus orci.
-
-
-          </div>
-        </PrincipalsContainer>
       </SubHeadingContents>
     </AppContainer>
   );
