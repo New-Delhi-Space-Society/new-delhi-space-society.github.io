0d9be2420ca2eb3ad17d2ed7d3b494c563c25dbd
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-08-16 16:53:49 +0530

    Update SpacehackContents.tsx

--- components/spacehack/SpacehackContents.tsx
+++ components/spacehack/SpacehackContents.tsx
@@ -46,11 +46,10 @@ export default function SpacehackContents() {
       <SubHeadingContents title="SpaceHack@ESYA">
         <HistoryContainer>
           <div className="content" style={{ marginRight: "4px" }}>
-          Progress in technology means pushing the boundaries of what is possible, not only on earth, but also the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions which threaten their survival, computer systems and technology too evolve and become more sophisticated if subjected to extraordinary circumstances. And what is a more strenuous, more demanding environment than space? With this in mind, Exun Clan collaborates with the New Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
 
           SpaceHack@ESYA 2023 is a fast-paced aerospace-focused startup pitch competition, that puts you into the suits of the heads of startups, competing to develop your product, bring it to market, and grow the company as if the sky is not the limit.
 
-Will your crew reach the stars, burn up in the atmosphere, and go out with a boom as all your skills are tested, including innovation, design, marketing, and pitching? Sign up at <a href="https://unstop.com/p/spacehack-esya23-indraprastha-institute-of-information-technology-iiit-delhi-728723"> Unstop </a>, don your suits, check all safety systems, and prepare for lift-off on the 25th of August 2023 at IIIT Delhi!
+Will your crew reach the stars, burn up in the atmosphere, and go out with a boom as all your skills are tested, including innovation, design, marketing, and pitching? Sign up at <a href="https://unstop.com/p/spacehack-esya23-indraprastha-institute-of-information-technology-iiit-delhi-728723">Unstop</a>, don your suits, check all safety systems, and prepare for lift-off on the 25th of August 2023 at IIIT Delhi!
           
             <br />
             <br />
