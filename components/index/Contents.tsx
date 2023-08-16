8dbc6fb9569d710f2e6cd13246f7bc5ed36674b6
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-08-17 00:26:24 +0530

    Update Contents.tsx

--- components/index/Contents.tsx
+++ components/index/Contents.tsx
@@ -11,13 +11,9 @@ export default function Contents() {
       <a href={nssUrl} target="_blank" rel="noopener noreferrer">
          National Space Society
       </a>
-      . Our mission is to conduct outreach programs through a variety, participate in space events at a global stage and lend our
-       voices to the conversation, 
-       and host various 
-       activities to fulfill our objectives and promote space awareness.
-        As our organization continues to grow, we warmly welcome your support!
-         Discover more about us in the About Us section and join us in our journey of
-          exploration and discovery, we hope to see you with us among the stars 🚀
+      . Our mission is to make space, its resources, and its opportunities accessible to all. We conduct outreach programs through various mediums, participate in space events at a global stage and lend our voices to the conversation, and host activities to fulfill our objectives and promote space awareness. 
+
+As our organization continues to grow, we warmly welcome your support! Discover more about us in the About Us section and join us in our journey of exploration and discovery. We hope to see you with us among the stars 🚀 .
     </>
   );
 
