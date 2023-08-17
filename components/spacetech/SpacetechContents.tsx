65a87802648ff6e187a609f8298854f93ef659f7
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-08-17 15:52:54 +0530

    Update SpacetechContents.tsx

--- components/spacetech/SpacetechContents.tsx
+++ components/spacetech/SpacetechContents.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import AppContainer from "../AppContainer";
 import SubHeadingContents from "../SubHeadingContents";
 import { device } from "../../ThemeConfig";
-import ImageGrid from "../imagegrid"; 
 
 const HistoryContainer = styled.div`
   display: flex;
@@ -34,6 +33,23 @@ const PrincipalsContainer = styled.div`
   }
 `;
 
+const ImageGridContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+`;
+
+const ImageGridItem = styled.div`
+  flex: 1 0 0;
+  max-width: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* or object-fit: contain */
+  }
+`;
+
 const firstRowImages = [
   "https://media.discordapp.net/attachments/1140692334653214913/1140693384504299610/Screenshot_2023-08-14_at_4.13.49_PM.png?width=1181&height=662",
   "https://media.discordapp.net/attachments/1140692334653214913/1140693385120845884/Screenshot_2023-08-14_at_4.14.42_PM.png?width=1170&height=662",
@@ -49,6 +65,7 @@ const secondRowImages = [
   "https://cdn.discordapp.com/attachments/1140692334653214913/1140693490766987415/Screenshot_2023-08-14_at_5.11.31_PM.png",
   "https://media.discordapp.net/attachments/1140692334653214913/1140752611226693662/Copy_of_Neospace_CORE_DPSDW_Team_One_Spacetech_Finals_-_Partha_Khurana_page-0001_1.jpg?width=468&height=662",
 ];
+
 const SpacetechContents: React.FC = () => {
   return (
     <AppContainer>
@@ -56,7 +73,7 @@ const SpacetechContents: React.FC = () => {
         <HistoryContainer>
           <div className="content" style={{ marginRight: "4px" }}>
             <p>
-              Progress in technology means pushing the boundaries of what is possible, not only on earth, but also the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions which threaten their survival, computer systems and technology too evolve and become more sophisticated if subjected to extraordinary circumstances. And what is a more strenuous, more demanding environment than space? With this in mind, Exun Clan collaborates with the New Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
+              Progress in technology means pushing the boundaries of what is possible, not only on earth, but also the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions which threaten their survival, computer systems and technology too evolve and become more sophisticated if subjected to extraordinary circumstances. And what is a more strenuous, more demanding environment than space? With this in mind, Exun Clan collaborates with theNew Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
             </p>
    
           </div>
@@ -64,13 +81,21 @@ const SpacetechContents: React.FC = () => {
       </SubHeadingContents>
       <SubHeadingContents title="Past Competitions (2018-2022)">
         <PrincipalsContainer>
-          <div className="content">
-      <p>  </p>
-          </div>
-          <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "16px" }}>
-          <ImageGrid images={firstRowImages} />
-          <ImageGrid images={secondRowImages} />
-          </div>
+          <div className="content"></div>
+          <ImageGridContainer>
+            {firstRowImages.map((image, index) => (
+              <ImageGridItem key={index}>
+                <img src={image} alt={`Image ${index + 1}`} />
+              </ImageGridItem>
+            ))}
+          </ImageGridContainer>
+          <ImageGridContainer>
+            {secondRowImages.map((image, index) => (
+              <ImageGridItem key={index}>
+                <img src={image} alt={`Image ${index + 6}`} />
+              </ImageGridItem>
+            ))}
+          </ImageGridContainer>
         </PrincipalsContainer>
       </SubHeadingContents>
     </AppContainer>
