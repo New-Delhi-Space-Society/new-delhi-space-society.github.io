19e59048c9e3559aaba5bb9e228a89bddaacd05b
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-08-21 02:09:16 +0530

    Update Contents.tsx

--- components/index/Contents.tsx
+++ components/index/Contents.tsx
@@ -4,13 +4,6 @@ import SubHeadingContents from "../SubHeadingContents";
 import { nssUrl } from "../../config";
 import { KBody } from "../Typography";
 
-interface Styles {
-  centeredContainer: React.CSSProperties;
-  badgeContainer: React.CSSProperties;
-  badgeImage: React.CSSProperties;
-  logoImage: React.CSSProperties;
-}
-
 export default function Contents() {
   const introductionContent = (
     <>
@@ -18,9 +11,9 @@ export default function Contents() {
       <a href={nssUrl} target="_blank" rel="noopener noreferrer">
         National Space Society
       </a>
-      . Our mission is to make space, its resources, and its opportunities accessible to all. We conduct outreach programs through various mediums, participate in space events at a global stage, lend our voices to the conversation, and host activities to fulfill our objectives and promote space awareness.
+      . Our mission is to make space, its resources, and its opportunities accessible to all. We conduct outreach programs through various mediums, participate in space events at a global stage and lend our voices to the conversation, and host activities to fulfill our objectives and promote space awareness. 
 
-      As our organization continues to grow, we warmly welcome your support! Discover more about us in the About Us section and join us on our journey of exploration and discovery. We hope to see you among the stars 🚀.
+      As our organization continues to grow, we warmly welcome your support! Discover more about us in the About Us section and join us in our journey of exploration and discovery. We hope to see you with us among the stars 🚀 .
     </>
   );
 
@@ -30,28 +23,43 @@ export default function Contents() {
         <KBody>{introductionContent}</KBody>
       </SubHeadingContents>
       <SubHeadingContents title="Features & Awards">
-        <div style={styles.centeredContainer}>
-          <div style={styles.badgeContainer}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              margin: "20px 0",
+            }}
+          >
             <img
               src="https://cdn.dribbble.com/users/2570/screenshots/17518521/media/a15126d45bf6c62753d781dbb823e36b.png"
               alt="Badge 1"
-              style={styles.badgeImage}
+              width="150"
+              style={{ marginBottom: "10px" }}
             />
             <img
               src="https://cdn.dribbble.com/users/2570/screenshots/17518521/media/a15126d45bf6c62753d781dbb823e36b.png"
               alt="Badge 2"
-              style={{ ...styles.badgeImage, marginLeft: "20px" }}
+              width="150"
+              style={{ marginBottom: "10px" }}
             />
             <a
               href="https://www.hindustantimes.com/cities/delhi-news/asteroid-day-young-space-enthusiasts-shine-bright-in-delhi-s-skylight-101656603645351.html"
               target="_blank"
               rel="noopener noreferrer"
-              style={{ ...styles.badgeImage, marginLeft: "20px" }}
+              style={{ marginTop: "10px" }}
             >
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Hindustan_Times_logo.svg"
                 alt="Hindustan Times Logo"
-                style={styles.logoImage}
+                width="150"
               />
             </a>
           </div>
@@ -60,23 +68,3 @@ export default function Contents() {
     </AppContainer>
   );
 }
-
-const styles: Styles = {
-  centeredContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column" as const, 
-  }
-  badgeContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  badgeImage: {
-    width: "150px",
-  },
-  logoImage: {
-    width: "300px",
-  },
-};
