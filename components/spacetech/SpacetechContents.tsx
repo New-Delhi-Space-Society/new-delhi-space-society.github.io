3ad1bf1d2f644fad7aa57170c91305895a35e036
Kavin Valli <mail@kavin.me>
2023-08-22 16:01:52 +0530

    spacetech grid

--- components/spacetech/SpacetechContents.tsx
+++ components/spacetech/SpacetechContents.tsx
@@ -33,23 +33,25 @@ const PrincipalsContainer = styled.div`
   }
 `;
 
-const ImageGridContainer = styled.div`
+const FirstImageGridContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 2px;
-  margin-top: 16px; 
-  .image-grid-container {
-    margin-top: 16px;
+  margin-top: 16px;
+
+  img {
+    height: 200px;
   }
 `;
 
-const ImageGridItem = styled.div`
-  flex: 1 0 0;
-  max-width: 100%;
+const SecondImageGridContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2px;
+  margin-top: 16px;
+
   img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover; 
+    height: 350px;
   }
 `;
 
@@ -76,39 +78,37 @@ const SpacetechContents: React.FC = () => {
         <HistoryContainer>
           <div className="content" style={{ marginRight: "4px" }}>
             <p>
-              Progress in technology means pushing the boundaries of what is possible, not only on earth, but also the final frontier — space. Just as biological systems undergo evolution when subjected to strenuous conditions which threaten their survival, computer systems and technology too evolve and become more sophisticated if subjected to extraordinary circumstances. And what is a more strenuous, more demanding environment than space? With this in mind, Exun Clan collaborates with theNew Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology. The event typically has multiple challenging rounds, building up to a final presentation round — with a twist!
+              Progress in technology means pushing the boundaries of what is
+              possible, not only on earth, but also the final frontier — space.
+              Just as biological systems undergo evolution when subjected to
+              strenuous conditions which threaten their survival, computer
+              systems and technology too evolve and become more sophisticated if
+              subjected to extraordinary circumstances. And what is a more
+              strenuous, more demanding environment than space? With this in
+              mind, Exun Clan collaborates with theNew Delhi Chapter of the
+              prestigious National Space Society (USA) to give students the
+              opportunity to explore the realms of space and its concurrence
+              with technology. The event typically has multiple challenging
+              rounds, building up to a final presentation round — with a twist!
             </p>
-   
           </div>
         </HistoryContainer>
       </SubHeadingContents>
       <SubHeadingContents title="Past Competitions (2018-2022)">
-      <PrincipalsContainer>
+        <PrincipalsContainer>
           <div className="content"></div>
-          <div className="image-grid-container"> 
-            <ImageGridContainer>
-              {firstRowImages.map((image, index) => (
-                <ImageGridItem key={index}>
-                  <img src={image} alt={`Image ${index + 1}`} />
-                </ImageGridItem>
-              ))}
-            </ImageGridContainer>
-          </div>
-          <p>
-          ‎‎‎‎
-          </p>
-          <p>
-          ‎‎‎‎
-          </p>
-          <div className="image-grid-container">
-            <ImageGridContainer>
-              {secondRowImages.map((image, index) => (
-                <ImageGridItem key={index}>
-                  <img src={image} alt={`Image ${index + 6}`} />
-                </ImageGridItem>
-              ))}
-            </ImageGridContainer>
-          </div>
+          <FirstImageGridContainer>
+            {firstRowImages.map((image, index) => (
+              <img key={index} src={image} alt={`Image ${index + 1}`} />
+            ))}
+          </FirstImageGridContainer>
+          <p>‎‎‎‎</p>
+          <p>‎‎‎‎</p>
+          <SecondImageGridContainer>
+            {secondRowImages.map((image, index) => (
+              <img key={index} src={image} alt={`Image ${index + 6}`} />
+            ))}
+          </SecondImageGridContainer>
         </PrincipalsContainer>
       </SubHeadingContents>
     </AppContainer>
