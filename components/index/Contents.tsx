bd14a0f47f06f3be5af6473f1c974c8bacf68dc5
Kavin Valli <mail@kavin.me>
2023-08-22 15:53:54 +0530

    make home page featured responsive

--- components/index/Contents.tsx
+++ components/index/Contents.tsx
@@ -3,17 +3,32 @@ import AppContainer from "../AppContainer";
 import SubHeadingContents from "../SubHeadingContents";
 import { nssUrl } from "../../config";
 import { KBody } from "../Typography";
+import styled from "styled-components";
+
+const FeaturedDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+`;
 
 export default function Contents() {
   const introductionContent = (
     <>
-      With the aim of advancing space science and engineering and taking humanity to the stars, we are a passionate group of space enthusiasts at the New Delhi Space Society, a distinguished award-winning chapter of the{" "}
+      With the aim of advancing space science and engineering and taking
+      humanity to the stars, we are a passionate group of space enthusiasts at
+      the New Delhi Space Society, a distinguished award-winning chapter of the{" "}
       <a href={nssUrl} target="_blank" rel="noopener noreferrer">
         National Space Society
       </a>
-      . Our mission is to make space, its resources, and its opportunities accessible to all. We conduct outreach programs through various mediums, participate in space events at a global stage and lend our voices to the conversation, and host activities to fulfill our objectives and promote space awareness. 
-
-      As our organization continues to grow, we warmly welcome your support! Discover more about us in the About Us section and join us in our journey of exploration and discovery. We hope to see you with us among the stars 🚀 .
+      . Our mission is to make space, its resources, and its opportunities
+      accessible to all. We conduct outreach programs through various mediums,
+      participate in space events at a global stage and lend our voices to the
+      conversation, and host activities to fulfill our objectives and promote
+      space awareness. As our organization continues to grow, we warmly welcome
+      your support! Discover more about us in the About Us section and join us
+      in our journey of exploration and discovery. We hope to see you with us
+      among the stars 🚀 .
     </>
   );
 
@@ -23,47 +38,32 @@ export default function Contents() {
         <KBody>{introductionContent}</KBody>
       </SubHeadingContents>
       <SubHeadingContents title="Features & Awards">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              margin: "20px 0",
-            }}
+        <FeaturedDiv>
+          <img
+            src="https://cdn.dribbble.com/users/2570/screenshots/17518521/media/a15126d45bf6c62753d781dbb823e36b.png"
+            alt="Badge 1"
+            width="150"
+            style={{ marginBottom: "10px" }}
+          />
+          <img
+            src="https://cdn.dribbble.com/users/2570/screenshots/17518521/media/a15126d45bf6c62753d781dbb823e36b.png"
+            alt="Badge 2"
+            width="150"
+            style={{ marginBottom: "10px" }}
+          />
+          <a
+            href="https://www.hindustantimes.com/cities/delhi-news/asteroid-day-young-space-enthusiasts-shine-bright-in-delhi-s-skylight-101656603645351.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginTop: "10px" }}
           >
             <img
-              src="https://cdn.dribbble.com/users/2570/screenshots/17518521/media/a15126d45bf6c62753d781dbb823e36b.png"
-              alt="Badge 1"
+              src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Hindustan_Times_logo.svg"
+              alt="Hindustan Times Logo"
               width="150"
-              style={{ marginBottom: "10px" }}
             />
-            <img
-              src="https://cdn.dribbble.com/users/2570/screenshots/17518521/media/a15126d45bf6c62753d781dbb823e36b.png"
-              alt="Badge 2"
-              width="150"
-              style={{ marginBottom: "10px" }}
-            />
-            <a
-              href="https://www.hindustantimes.com/cities/delhi-news/asteroid-day-young-space-enthusiasts-shine-bright-in-delhi-s-skylight-101656603645351.html"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ marginTop: "10px" }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Hindustan_Times_logo.svg"
-                alt="Hindustan Times Logo"
-                width="150"
-              />
-            </a>
-          </div>
-        </div>
+          </a>
+        </FeaturedDiv>
       </SubHeadingContents>
     </AppContainer>
   );
